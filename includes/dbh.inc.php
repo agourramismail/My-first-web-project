0184d9a47b1b788fd3c1bdb25bbc9e0db46ec461
agourramismail <pinitonft60@gmail.com>
2024-11-29 11:04:08 +0000

    html modification

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -8,6 +8,6 @@ try {
     $pdo= new PDO("mysql:host=$host;dbname=$dbname",$dbusername,$dbpassword);
     $pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
 } catch (PDOException $e) {
-    die("db mkhedamach" . $e->getMessage());
+    die(" conexion dial la base de données mkhedamach" . $e->getMessage());
 }
 ?>
--- index.php
+++ index.php
@@ -22,11 +22,11 @@ include ('footer.php');
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" required>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Email address" required>
     </div>
     <div class="mb-3">
         <label for="pwd" class="form-label">Password</label>
-        <input type="password" class="form-control" id="pwd" name="pwd" required>
+        <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Password" required>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
