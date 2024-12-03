4e07fd3078ec77ca6dee30ab1d393eef665c6c89
agourramismail <pinitonft60@gmail.com>
2024-12-03 10:17:49 +0000

    midfier config session

--- includes/config_session_inc.php
+++ includes/config_session_inc.php
@@ -15,10 +15,15 @@ session_start();
 if(isset($_SESSION["user_id"])){
     if(!isset($_SESSION["last_regeneration"])){
         regenerate_session_id_loggedin();
+    }else{
+        $interval= 60*30;
+        if(time() - $_SESSION["last_regeneration"] >= $interval){
+            regenerate_session_id_loggedin();
+        }
     }
 }else{
     if(!isset($_SESSION["last_regeneration"])){
-        regenerate_session_id_loggedin();
+        regenerate_session_id();
     }else {
         $interval= 60*30;
         if(time() - $_SESSION["last_regeneration"] >= $interval){
--- includes/login_view.inc.php
+++ includes/login_view.inc.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 function output_username(){
     if(isset($_SESSION["user_id"])){
-        echo "you are logged in as " . $_SESSION["user_id"];
+        echo "you are logged in as " . $_SESSION["user_username"];
     }else{
         echo "you are not logged in";
     }
