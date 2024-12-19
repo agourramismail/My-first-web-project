647d36b4bc98535d118d8c7b5c563b78d4a28fb8
agourramismail <pinitonft60@gmail.com>
2024-12-19 20:52:08 +0100

    adding errors to update_user

--- includes/update_dashboard.inc.php
+++ includes/update_dashboard.inc.php
@@ -9,7 +9,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_user'])) {
     $id =$_POST['id'];
     $username = $_POST['nom'];
     $email = $_POST['email'];
-    $password = $_POST['pwd']; 
+    $password = $_POST['pwd'];
+    require_once '../controls/dashboard_contr.inc.php'; 
+
+    // gestion dial les erreurs
+    $errors = [];
+
+
+    if (is_input_empty($username, $email, $password) !== false) {
+        $errors["empty_input"] = "Please fill in all fields.";
+    }
+    if (is_email_invalid($email)) {
+        $errors["invalid_email"] = "Invalid email.";
+    }
+    if (is_username_taken($pdo, $username)) {
+        $errors["username_taken"] = "Username already taken.";
+    }
+    if (is_email_registred($pdo, $email)) {
+        $errors["email_registred"] = "Email already registered.";
+    }
+
+
+    require_once 'config_session_inc.php';
+
+
+    if ($errors) {
+        $_SESSION["errors_signup"] = $errors;
+        header("Location: ../dashboard.php");
+        exit();
+    }
 
     if (update_users($pdo, $id, $username, $email, $password)) {
         header("Location: ../dashboard.php?edit=success");
--- models/dashboard_model.inc.php
+++ models/dashboard_model.inc.php
@@ -43,5 +43,20 @@ function delete_user(object $pdo, int $id): bool {
 
     return $stmt->execute();
 }
+function get_username(object $pdo, string $username) {
+    $query="SELECT nom FROM users WHERE nom = :nom ";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":nom", $username);
+    $stmt-> execute();
+}
+function get_email(object $pdo, string $email) {
+    $query="SELECT nom FROM users WHERE email = :email ";
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam(":email", $email);
+    $stmt-> execute();
+
+    $result= $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
 
 
